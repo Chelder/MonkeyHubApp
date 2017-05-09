d6a7acf9da71cfba811ca9cb57410ee7c0a67372
Chelder <chelder.lins@gmail.com>
2017-05-09 15:24:39 -0300

    Implementando INotifyPropertyChanged. Caso mais simples

--- MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MonkeyHubApp.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Descricao { get; set; }
 
         public MainViewModel()
         {
             Descricao = "Ola mundo! Eu estou aqui!";
+
+            Task.Delay(3000).ContinueWith(t =>
+            {
+                Descricao = "Meu texto Mudou";
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Descricao"));
+                }
+            });
         }
     }
 }
