02938c29d0ace87733f795a9302ce5551ac5fe4a
Chelder <chelder.lins@gmail.com>
2017-05-09 15:39:06 -0300

    Implementando INotifyPropertyChanged. Implementado classe BaseViewModel

--- MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,45 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace MonkeyHubApp.ViewModels
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : BaseViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(storage, value))
-            {
-                return false;
-            }
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-
-            return true;
-        }
 
         private string _descricao;
         public string Descricao
         {
             get { return _descricao; }
-            set
-            {
-                SetProperty(ref _descricao, value);
-            }
+            set { SetProperty(ref _descricao, value); }
         }
-        
+
         public MainViewModel()
         {
             Descricao = "Ola mundo! Eu estou aqui!";
