9c34516bf9fb8962711c8fefa9bcd5d969180e67
Chelder <chelder.lins@gmail.com>
2017-05-10 10:56:49 -0300

    Classe e coleções - Trabalhando com coleções: implementado um ObservableCollection.

--- MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -22,16 +23,20 @@ namespace MonkeyHubApp.ViewModels
             }
         }
 
+        public ObservableCollection<string> Resultados { get; }
+
         public Command SearhCommand { get; }
 
         public MainViewModel()
         {
             SearhCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
+
+            Resultados = new ObservableCollection<string>(new[] { "abc", "cde", "1", "2", "3", "4" });
         }
 
         async void ExecuteSearchCommand()
         {
-            await Task.Delay(2000);
+            //await Task.Delay(2000);
 
             //Forma errada de utilizar o DisplayAlert
             bool res = await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Você pesquisou por '{SearchTerm}'?", "Sim", "Não");
@@ -40,7 +45,9 @@ namespace MonkeyHubApp.ViewModels
             {
                 //Forma errada de utilizar o DisplayAlert
                 await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "Obrigado", "Ok");
-            } else
+                Resultados.Add(SearchTerm);
+            }
+            else
             {
                 //Forma errada de utilizar o DisplayAlert
                 await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "De nada", "Ok");
